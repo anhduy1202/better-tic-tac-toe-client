f27e9243ee3d7df57b8f9ae1cbcc70e329c1854b
Daniel Truong <aduy1122@gmail.com>
2021-08-26 18:05:57 -0700

    Update firebase.js

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -9,14 +9,14 @@ const messID = process.env.REACT_APP_MESS_ID;
 const appId = process.env.REACT_APP_APP_ID;
 const measurementId = process.env.REACT_APP_MEASUREMENT_ID;
 const firebaseConfig = {
-    apiKey: apiKey,
-    authDomain: authDomain,
-    databaseURL: dbUrl,
-    projectId: projectId,
-    storageBucket: sb,
-    messagingSenderId: messID,
-    appId: appId,
-    measurementId: measurementId
+    apiKey: "AIzaSyDLGHUTTcGB20yvsUvN-KQiy7o4_JyE-WU",
+    authDomain:  "better-tic-tac-toe.firebaseapp.com",
+    databaseURL: "https://better-tic-tac-toe-default-rtdb.firebaseio.com",
+    projectId:  "better-tic-tac-toe",
+    storageBucket:  "better-tic-tac-toe.appspot.com",
+    messagingSenderId:  "650136148230",
+    appId: "1:650136148230:web:87825349146e48e07da687",
+    measurementId:  "G-QMB8CZN9DH"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
