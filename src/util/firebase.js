63b4efd62961f6cb4176939873c7b731d1584b3a
Daniel Truong <aduy1122@gmail.com>
2021-08-26 21:55:27 -0700

    Update firebase.js

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -4,7 +4,7 @@ import firebase from "firebase";
 const firebaseConfig = {
     apiKey:  process.env.REACT_APP_API_KEY,
     authDomain:  process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: "https://better-tic-tac-toe-default-rtdb.firebaseio.com",
+    databaseURL: process.env.REACT_APP_DB_URL,
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket:process.env.REACT_APP_SB,
     messagingSenderId: process.env.REACT_APP_MESS_ID,
@@ -13,4 +13,5 @@ const firebaseConfig = {
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+  
   export default firebase;
