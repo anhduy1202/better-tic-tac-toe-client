bff46786149b0cbfc57301abb4c6562a9b7352f7
Daniel Truong <aduy1122@gmail.com>
2021-08-26 21:21:28 -0700

    Update firebase.js

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -1,15 +1,8 @@
 import firebase from "firebase";
-require('dotenv').config();
-const apiKey = process.env.REACT_APP_API_KEY;
-const authDomain = process.env.REACT_APP_AUTH_DOMAIN;
-const dbUrl = process.env.REACT_APP_DB_URL;
-const projectId = process.env.REACT_APP_PROJECT_ID;
-const sb = process.env.REACT_APP_SB;
-const messID = process.env.REACT_APP_MESS_ID;
-const appId = process.env.REACT_APP_APP_ID;
-const measurementId = process.env.REACT_APP_MEASUREMENT_ID;
+
+
 const firebaseConfig = {
- apiKey:  process.env.REACT_APP_API_KEY,
+    apiKey:  process.env.REACT_APP_API_KEY,
     authDomain:  process.env.REACT_APP_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_DB_URL,
     projectId: process.env.REACT_APP_PROJECT_ID,
